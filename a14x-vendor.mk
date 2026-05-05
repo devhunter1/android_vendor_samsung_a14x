8149139cf3bd22d1fba3d29fea9047331a4324b2
devhunter1 <anubhabkundu2@gmail.com>
2026-05-05 09:59:03 +0530

    a14x: Vendor blobs from A146BXXUBDYL1

---
+++ a14x-vendor.mk
@@ -0,0 +1,94 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/samsung/a14x
+
+PRODUCT_COPY_FILES += \
+    vendor/samsung/a14x/proprietary/vendor/etc/floating_feature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/floating_feature.xml \
+    vendor/samsung/a14x/proprietary/vendor/etc/gnss/ca.pem:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/ca.pem \
+    vendor/samsung/a14x/proprietary/vendor/etc/gnss/deleteLogCommands:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/deleteLogCommands \
+    vendor/samsung/a14x/proprietary/vendor/etc/gnss/gps.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.cfg \
+    vendor/samsung/a14x/proprietary/vendor/etc/gnss/gps.debug.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.debug.cfg \
+    vendor/samsung/a14x/proprietary/vendor/etc/gnss/gps.issuetracker.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.issuetracker.cfg \
+    vendor/samsung/a14x/proprietary/vendor/etc/gnss/hardware_factors.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/hardware_factors.conf \
+    vendor/samsung/a14x/proprietary/vendor/etc/init/init.gps.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.gps.rc \
+    vendor/samsung/a14x/proprietary/vendor/firmware/APDV_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/APDV_AUDIO_SLSI.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/APMS_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/APMS_AUDIO_SLSI.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/AP_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/AP_AUDIO_SLSI.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/SoundBoosterParam.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/SoundBoosterParam.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/abov/a96t396_a14x_main.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/abov/a96t396_a14x_main.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/abov/a96t396_a14x_sub.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/abov/a96t396_a14x_sub.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/abox_solution.conf:$(TARGET_COPY_OUT_VENDOR)/firmware/abox_solution.conf \
+    vendor/samsung/a14x/proprietary/vendor/firmware/aw882xx_pid_2055a_acf.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw882xx_pid_2055a_acf.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/aw882xx_pid_2055b_acf.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw882xx_pid_2055b_acf.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/calliope_sram.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/calliope_sram.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/ili7807_a14x.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/ili7807_a14x.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/mfc_fw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/mfc_fw.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/nt36672_a14x_2ndbr_tianma.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nt36672_a14x_2ndbr_tianma.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/nt36672_a14x_2ndbr_tianma_mp.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nt36672_a14x_2ndbr_tianma_mp.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/nt36672_a14x_tianma.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nt36672_a14x_tianma.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/nt36672_a14x_tianma_mp.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nt36672_a14x_tianma_mp.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/os.checked.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/os.checked.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/pablo_icpufw.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/pablo_icpufw.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/rxse.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/rxse.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/sectiongraph_tplg.conf:$(TARGET_COPY_OUT_VENDOR)/firmware/sectiongraph_tplg.conf \
+    vendor/samsung/a14x/proprietary/vendor/firmware/setfile_3l6.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_3l6.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/setfile_gc02m1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_gc02m1.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/setfile_gc02m2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_gc02m2.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/setfile_hi1336.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_hi1336.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/setfile_jn1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_jn1.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/setfile_sc201.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_sc201.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/setfile_sc201_macro.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/setfile_sc201_macro.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/spkamp_aw882xx.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/spkamp_aw882xx.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/spkamp_param_aw882xx.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/spkamp_param_aw882xx.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/td4160_a13x_boe.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/td4160_a13x_boe.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/txse1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/txse1.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/txse2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/txse2.bin \
+    vendor/samsung/a14x/proprietary/vendor/firmware/wifi/hydra_config.sdb:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/hydra_config.sdb \
+    vendor/samsung/a14x/proprietary/vendor/firmware/wifi/mx140_bt.hcf:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/mx140_bt.hcf \
+    vendor/samsung/a14x/proprietary/vendor/firmware/wifi/mx140_wlan.hcf:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/mx140_wlan.hcf \
+    vendor/samsung/a14x/proprietary/vendor/firmware/wifi/mx140_wlan_t.hcf:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/mx140_wlan_t.hcf \
+    vendor/samsung/a14x/proprietary/vendor/firmware/wifi/platform.txt:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/platform.txt \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-000000534b4d:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-000000534b4d \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-0000534b504d:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-0000534b504d \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-0050524f4341:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-0050524f4341 \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-0053545354ab:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-0053545354ab \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-00575644524d:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-00575644524d \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-42494f535542:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-42494f535542 \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-46494e474502:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-46494e474502 \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-4662436b6d52:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-4662436b6d52 \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-474154454b45:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-474154454b45 \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-4b45594d5354:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-4b45594d5354 \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-505256544545:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-505256544545 \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-5345435f4652:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-5345435f4652 \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-564c544b5052:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-564c544b5052 \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-656e676d6f64:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-656e676d6f64 \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-6b6e78677564:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-6b6e78677564 \
+    vendor/samsung/a14x/proprietary/vendor/tee/00000000-0000-0000-0000-6d73745f5441:$(TARGET_COPY_OUT_VENDOR)/tee/00000000-0000-0000-0000-6d73745f5441 \
+    vendor/samsung/a14x/proprietary/vendor/tee/driver/00000000-0000-0000-0000-494363447256:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-0000-0000-0000-494363447256 \
+    vendor/samsung/a14x/proprietary/vendor/tee/driver/00000000-0000-0000-0000-4d53546d7374:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-0000-0000-0000-4d53546d7374 \
+    vendor/samsung/a14x/proprietary/vendor/tee/driver/00000000-0000-0000-0000-53626f786476:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-0000-0000-0000-53626f786476 \
+    vendor/samsung/a14x/proprietary/vendor/tee/driver/00000000-0000-0000-0000-564c544b4456:$(TARGET_COPY_OUT_VENDOR)/tee/driver/00000000-0000-0000-0000-564c544b4456
+
+PRODUCT_PACKAGES += \
+    libeis_core \
+    libuniplugin \
+    libvdis_interface \
+    libvra \
+    sensors.grip \
+    sensors.inputvirtual \
+    sensors.sensorhub \
+    camera.s5e8535 \
+    libIRTA \
+    libdlrmsc \
+    libomp \
+    libremosaic_wrapper \
+    libremosaiclib \
+    libsec2lsi_conversion \
+    libsensorlistener \
+    libuniapi \
+    libvpl \
+    vendor.samsung.hardware.thermal@1.0 \
+    gpsd
